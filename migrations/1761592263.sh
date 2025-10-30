9298fc0d255c64ba858bdc042edcfbdba236ba18
Samuel Simons <47101903+kromsam@users.noreply.github.com>
2025-10-30 21:35:25 +0100

    use yay to install gazelle-tui

--- migrations/1761592263.sh
+++ migrations/1761592263.sh
@@ -4,7 +4,7 @@ echo "Replace Impala with Gazelle"
 omarchy-pkg-drop impala
 
 if omarchy-cmd-missing gazelle; then
-  omarchy-pkg-add gazelle-tui
+  yay -S --noconfirm gazelle-tui
   omarchy-restart-waybar
 fi
 
