248a7bf6c35ebb8e8cc9a904a88e1a8fa2a03584
Patrick Roza <contact@patrickroza.com>
2025-11-17 14:50:56 +0100

    also add unique app ids to lazydocker and terminal editor

--- migrations/1763386443.sh
+++ migrations/1763386443.sh
@@ -1,4 +1,5 @@
-echo "Uniquely identify btop with custom app-id"
+echo "Uniquely identify terminal apps with custom app-ids"
 
 sed -i 's/\$terminal -e btop/$terminal --app-id=com.omarchy.btop -e btop/' ~/.config/hypr/bindings.conf
 sed -i 's/xdg-terminal-exec btop/xdg-terminal-exec --app-id=com.omarchy.btop btop/' ~/.config/waybar/config.jsonc
+sed -i 's/\$terminal -e lazydocker/$terminal --app-id=com.omarchy.lazydocker -e lazydocker/' ~/.config/hypr/bindings.conf
