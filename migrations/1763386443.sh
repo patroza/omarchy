9a7a33c3058150fe135382c283f12cff4cb8e059
Patrick Roza <contact@patrickroza.com>
2025-11-17 15:13:16 +0100

    introduce new omarchy-launch-tui that sets class.

--- migrations/1763386443.sh
+++ migrations/1763386443.sh
@@ -1,5 +1,6 @@
-echo "Uniquely identify terminal apps with custom app-ids"
+echo "Uniquely identify terminal apps with custom app-ids using omarchy-launch-tui"
 
-sed -i 's/\$terminal -e btop/$terminal --app-id=com.omarchy.btop -e btop/' ~/.config/hypr/bindings.conf
-sed -i 's/xdg-terminal-exec btop/xdg-terminal-exec --app-id=com.omarchy.btop btop/' ~/.config/waybar/config.jsonc
-sed -i 's/\$terminal -e lazydocker/$terminal --app-id=com.omarchy.lazydocker -e lazydocker/' ~/.config/hypr/bindings.conf
+sed -i 's/^\$terminal = uwsm-app -- xdg-terminal-exec$/$terminal = omarchy-launch-tui/' ~/.config/hypr/bindings.conf
+sed -i 's/xdg-terminal-exec btop/omarchy-launch-tui btop/' ~/.config/waybar/config.jsonc
+sed -i 's/\$terminal -e \([^ ]*\)/$terminal \1/g' ~/.config/hypr/bindings.conf
+
